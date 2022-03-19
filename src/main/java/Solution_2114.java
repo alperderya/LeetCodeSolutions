538804bcaa338341d7dc1b2241a44d1c8e03ebf1
Alper Derya <alper.derya@gmail.com>
2022-03-20 01:27:09 +0300

    2114.Maximum Number of Words Found in Sentences Problem Solution

---
+++ src/main/java/Solution_2114.java
@@ -0,0 +1,18 @@
+//LeetCode Maximum Number of Words Found in Sentences Problem Solution 2114
+public class Solution_2114 {
+
+    public static void main(String[] args) {
+        String[] sentences = new String[]{"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+        Solution_2114 sol = new Solution_2114();
+        System.out.println(sol.mostWordsFound(sentences));
+    }
+
+    public int mostWordsFound(String[] sentences) {
+        int maxValue = -1;
+        for(String s:sentences){
+            int numberOfWords = s.split(" ").length;
+            if(numberOfWords>maxValue) maxValue=numberOfWords;
+        }
+        return maxValue;
+    }
+}
